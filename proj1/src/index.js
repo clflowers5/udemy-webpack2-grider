7fd51f81d4cfc8a838062af5d99dc4edebdff0ea
Chris Flowers <clflowers5@gmail.com>
2017-05-21 21:17:30 -0600

    stuff

--- proj1/src/index.js
+++ proj1/src/index.js
@@ -1,4 +1,5 @@
-const sum = require('./sum');
+import sum from './sum';
+import './image_viewer'; // Does not export code
 
 const total = sum(10, 5);
 console.log(total);
--- proj1/webpack.config.js
+++ proj1/webpack.config.js
@@ -1,11 +1,46 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'dist/' // Used by loaders that use file paths
+  },
+  module: {
+    rules: [{
+      use: 'babel-loader',
+      test: /\.js$/
+    },
+      /*{
+       // Injects css through style tag
+       use: ['style-loader', 'css-loader'], // Applied right to left
+       test: /\.css$/
+       }*/
+      {
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
+        test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 40000
+            }
+          },
+          'image-webpack-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
